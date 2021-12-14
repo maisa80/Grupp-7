1f4e7998898e7ea5dbde3028bd2589662e5b16fc
maisa abusalem <heart.webd@gmail.com>
2021-12-14 14:38:31 +0100

    basic layout for all pages

---
+++ js/menu.js
@@ -0,0 +1,16 @@
+const toggle = document.querySelector(".toggle");
+const menu = document.querySelector(".menu");
+const items = document.querySelectorAll(".item");
+
+/* Toggle mobile menu */
+function toggleMenu() {
+  if (menu.classList.contains("active")) {
+    menu.classList.remove("active");
+    toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
+  } else {
+    menu.classList.add("active");
+    toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
+  }
+}
+/* Event Listener */
+toggle.addEventListener("click", toggleMenu, false);
